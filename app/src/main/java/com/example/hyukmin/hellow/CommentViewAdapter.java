d7124c54abb31463b8953a0110f036c4859ce072
hyukmmin <hyukmanni@naver.com>
2015-06-10 21:40:48 +0900

    detail view

---
+++ app/src/main/java/com/example/hyukmin/hellow/CommentViewAdapter.java
@@ -0,0 +1,56 @@
+package com.example.hyukmin.hellow;
+
+import android.app.Activity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by hyukmmin on 2015-06-05.
+ */
+public class CommentViewAdapter extends ArrayAdapter<String> {
+
+    private final Activity context;
+    private final String[] comment_list;
+    private final Integer[] comment_Img;
+    public CommentViewAdapter(Activity context, String[] comment_list, Integer[] comment_Img) {
+
+        super(context, R.layout.comment_list, comment_list);
+        this.context = context;
+        this.comment_list = comment_list;
+        this.comment_Img = comment_Img;
+
+    }
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView= inflater.inflate(R.layout.comment_list, null, true);
+
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.comment_list_text);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.comment_list_img);
+        TextView txtDate = (TextView) rowView.findViewById(R.id.comment_text_date);
+        /*
+        Random ran = new Random();
+        // 댓글 수만큼 이미지 리스트 랜덤 구성
+        for (int i = 0; i<=comment_Img.length;i++){
+
+            while (0<=comment_list.length) {
+                Integer index = (Integer) ran(comment_Img.hashCode());
+                Integer ran_img = (Integer) comment_Img[index];
+            }
+        }
+        */
+        txtTitle.setText(comment_list[position]);
+        Log.d("comment_Img[position]", ""+comment_Img[position]);
+        imageView.setImageResource(comment_Img[position]);
+        txtDate.setText("2015/06/07");
+        return rowView;
+    }
+}
--- app/src/main/java/com/example/hyukmin/hellow/DetailActivity.java
+++ app/src/main/java/com/example/hyukmin/hellow/DetailActivity.java
@@ -3,10 +3,16 @@ package com.example.hyukmin.hellow;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +29,9 @@ public class DetailActivity extends Activity {
     private ArrayList<String> childConName = null;
     private ArrayList<Integer> childConImg = null;
     private ExpandableListView ExListView;
+    private ScrollView main_SV;
+    private ScrollView sub_SV;
+    private ListView comment_LV;
 
     public String[] weatherName = {
             "맑음",
@@ -74,6 +83,168 @@ public class DetailActivity extends Activity {
 
     };
 
+    public String[] comment_list = {
+            "거기 날씨 어때요?",
+            "개구림",
+            "낙산 쪽은 괜찮아요",
+            "ㅎㅎ",
+            "저거 거짓말",
+            "뭐가 진짜임?",
+            "날씨 좋아용~",
+            "입수는 가능한가요?",
+            "네 다들 잘놀고 있어요~",
+            "숙소는 어디가 좋아요?",
+            "이 앞에 널린게 숙소임",
+            "아하",
+            "ㅋㅋㅋㅋㅋㅋ",
+            "거기 날씨 어때요?",
+            "개구림",
+            "낙산 쪽은 괜찮아요",
+            "ㅎㅎ",
+            "저거 거짓말",
+            "뭐가 진짜임?",
+            "날씨 좋아용~",
+            "입수는 가능한가요?",
+            "네 다들 잘놀고 있어요~",
+            "숙소는 어디가 좋아요?",
+            "이 앞에 널린게 숙소임",
+            "아하",
+            "ㅋㅋㅋㅋㅋㅋ",
+            "거기 날씨 어때요?",
+            "개구림",
+            "낙산 쪽은 괜찮아요",
+            "ㅎㅎ",
+            "저거 거짓말",
+            "뭐가 진짜임?",
+            "날씨 좋아용~",
+            "입수는 가능한가요?",
+            "네 다들 잘놀고 있어요~",
+            "숙소는 어디가 좋아요?",
+            "이 앞에 널린게 숙소임",
+            "아하",
+            "ㅋㅋㅋㅋㅋㅋ",
+            "거기 날씨 어때요?",
+            "개구림",
+            "낙산 쪽은 괜찮아요",
+            "ㅎㅎ",
+            "저거 거짓말",
+            "뭐가 진짜임?",
+            "날씨 좋아용~",
+            "입수는 가능한가요?",
+            "네 다들 잘놀고 있어요~",
+            "숙소는 어디가 좋아요?",
+            "이 앞에 널린게 숙소임",
+            "아하",
+            "ㅋㅋㅋㅋㅋㅋ",
+            "거기 날씨 어때요?",
+            "개구림",
+            "낙산 쪽은 괜찮아요",
+            "ㅎㅎ",
+            "저거 거짓말",
+            "뭐가 진짜임?",
+            "날씨 좋아용~",
+            "입수는 가능한가요?",
+            "네 다들 잘놀고 있어요~",
+            "숙소는 어디가 좋아요?",
+            "이 앞에 널린게 숙소임",
+            "아하",
+            "ㅋㅋㅋㅋㅋㅋ",
+            "거기 날씨 어때요?",
+            "개구림",
+            "낙산 쪽은 괜찮아요",
+            "ㅎㅎ",
+            "저거 거짓말",
+            "뭐가 진짜임?",
+            "날씨 좋아용~",
+            "입수는 가능한가요?",
+            "네 다들 잘놀고 있어요~",
+            "숙소는 어디가 좋아요?",
+            "이 앞에 널린게 숙소임",
+            "아하",
+            "ㅋㅋㅋㅋㅋㅋ"
+    };
+
+    Integer[] comment_Img = {
+            R.drawable.man1,
+            R.drawable.man2,
+            R.drawable.man3,
+            R.drawable.man4,
+            R.drawable.man5,
+            R.drawable.man6,
+            R.drawable.man1,
+            R.drawable.man2,
+            R.drawable.man3,
+            R.drawable.man4,
+            R.drawable.man5,
+            R.drawable.man6,
+            R.drawable.man1,
+            R.drawable.man1,
+            R.drawable.man2,
+            R.drawable.man3,
+            R.drawable.man4,
+            R.drawable.man5,
+            R.drawable.man6,
+            R.drawable.man1,
+            R.drawable.man2,
+            R.drawable.man3,
+            R.drawable.man4,
+            R.drawable.man5,
+            R.drawable.man6,
+            R.drawable.man1,
+            R.drawable.man1,
+            R.drawable.man2,
+            R.drawable.man3,
+            R.drawable.man4,
+            R.drawable.man5,
+            R.drawable.man6,
+            R.drawable.man1,
+            R.drawable.man2,
+            R.drawable.man3,
+            R.drawable.man4,
+            R.drawable.man5,
+            R.drawable.man6,
+            R.drawable.man1,
+            R.drawable.man1,
+            R.drawable.man2,
+            R.drawable.man3,
+            R.drawable.man4,
+            R.drawable.man5,
+            R.drawable.man6,
+            R.drawable.man1,
+            R.drawable.man2,
+            R.drawable.man3,
+            R.drawable.man4,
+            R.drawable.man5,
+            R.drawable.man6,
+            R.drawable.man1,
+            R.drawable.man1,
+            R.drawable.man2,
+            R.drawable.man3,
+            R.drawable.man4,
+            R.drawable.man5,
+            R.drawable.man6,
+            R.drawable.man1,
+            R.drawable.man2,
+            R.drawable.man3,
+            R.drawable.man4,
+            R.drawable.man5,
+            R.drawable.man6,
+            R.drawable.man1,
+            R.drawable.man1,
+            R.drawable.man2,
+            R.drawable.man3,
+            R.drawable.man4,
+            R.drawable.man5,
+            R.drawable.man6,
+            R.drawable.man1,
+            R.drawable.man2,
+            R.drawable.man3,
+            R.drawable.man4,
+            R.drawable.man5,
+            R.drawable.man6,
+            R.drawable.man1
+    };
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -93,7 +264,7 @@ public class DetailActivity extends Activity {
         ImageView today_img = (ImageView) findViewById(R.id.today_img);
         TextView today_text = (TextView) findViewById(R.id.today_text);
 
-        swim_img.setImageResource(R.drawable.swim);
+        swim_img.setImageResource(R.drawable.swim1);
         swim_text.setText(R.string.swim);
 
         today_img.setImageResource(R.drawable.image3);
@@ -114,8 +285,19 @@ public class DetailActivity extends Activity {
             childConName.add("" + weatherName[i]);
             childConImg.add(weatherImage[i]);
         }
-        childList_txt.add(childConName);
-        childList_img.add(childConImg);
+
+
+        int num = 0;
+        while (num<groupList.size()) {
+            childList_txt.add(childConName);
+            childList_img.add(childConImg);
+            num++;
+        }
+
+        // 댓글 작업(임시)
+        CommentViewAdapter adapter = new CommentViewAdapter(DetailActivity.this, comment_list, comment_Img);
+        comment_LV = (ListView)findViewById(R.id.comment_LV);
+        comment_LV.setAdapter(adapter);
 
         /*
         childListContent.add("1");
@@ -159,6 +341,93 @@ public class DetailActivity extends Activity {
             }
         });
 
+        main_SV = (ScrollView)findViewById(R.id.main_Scroll);
+
+        ExListView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                main_SV.requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });
+
+        sub_SV = (ScrollView)findViewById(R.id.sub_SV);
+
+        comment_LV.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                sub_SV.requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });
+        /*
+        comment_LV.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                sv.requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });
+        */
+        setListViewHeightBasedOnChildren(comment_LV);
+        setExpandableListViewHeight(ExListView, -1);
+
+    }
+
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+
+        if (listAdapter == null) {
+            // pre-condition
+            return;
+        }
+
+        int totalHeight = 0;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
+
+    }
+
+    private void setExpandableListViewHeight(ExpandableListView expandableListView, int group) {
+        ExpandableListAdapter listAdapter = expandableListView.getExpandableListAdapter();
+        if (listAdapter == null) {
+            return;
+        }
+
+        int desiredWidth = View.MeasureSpec.makeMeasureSpec(expandableListView.getWidth(), View.MeasureSpec.UNSPECIFIED);
+        int totalHeight = 0;
+        View view = null;
+        for (int i = 0; i < listAdapter.getGroupCount(); i++) {
+            view = listAdapter.getGroupView(i, false, view, expandableListView);
+            if (i == 0) {
+                view.setLayoutParams(new ViewGroup.LayoutParams(desiredWidth, ViewGroup.LayoutParams.WRAP_CONTENT));
+            }
+            view.measure(desiredWidth, View.MeasureSpec.UNSPECIFIED);
+            totalHeight += view.getMeasuredHeight();
+            if(((expandableListView.isGroupExpanded(i)) && (i != group)) || ((!expandableListView.isGroupExpanded(i)) && (i == group))) {
+                View listItem = null;
+                for (int j = 0; j < listAdapter.getChildrenCount(i); j++) {
+                    listItem = listAdapter.getChildView(i, j, false, listItem, expandableListView);
+                    listItem.setLayoutParams(new ViewGroup.LayoutParams(desiredWidth, View.MeasureSpec.UNSPECIFIED));
+                    listItem.measure(
+                            View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                            View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+                    totalHeight += listItem.getMeasuredHeight();
+                }
+            }
+        }
+        ViewGroup.LayoutParams params = expandableListView.getLayoutParams();
+        params.height = totalHeight + (expandableListView.getDividerHeight() * (listAdapter.getGroupCount() - 1));
+        expandableListView.setLayoutParams(params);
+        expandableListView.requestLayout();
     }
 
     private void setLayout(){
